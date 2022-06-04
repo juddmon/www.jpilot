3f756bc4d112d6dced95282e39b9c336e62e8e10
Judd Montgomery <judd@jpilot.org>
2022-06-04 14:48:16 -0400

    Update 2 for 2022-06-04

--- packages/focal/DOWNLOAD.sh
+++ packages/focal/DOWNLOAD.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-wget http://www.jpilot.org/ubuntu-20.04/jpilot_1.8.2-2_amd64.deb
-wget http://www.jpilot.org/ubuntu-20.04/jpilot_2.0.1-1_amd64.deb
-wget http://www.jpilot.org/ubuntu-20.04/jpilot-plugins_1.8.2-2_amd64.deb
-wget http://www.jpilot.org/ubuntu-20.04/jpilot-plugins_2.0.1-1_amd64.deb
-wget http://www.jpilot.org/ubuntu-20.04/libpisock9_0.13.0_amd64.deb
-wget http://www.jpilot.org/ubuntu-20.04/libpisync1_0.13.0_amd64.deb
-wget http://www.jpilot.org/ubuntu-20.04/pilot-link_0.13.0_amd64.deb
+wget http://www.jpilot.org/packages/focal/jpilot_1.8.2-2_amd64.deb
+wget http://www.jpilot.org/packages/focal/jpilot_2.0.1-1_amd64.deb
+wget http://www.jpilot.org/packages/focal/jpilot-plugins_1.8.2-2_amd64.deb
+wget http://www.jpilot.org/packages/focal/jpilot-plugins_2.0.1-1_amd64.deb
+wget http://www.jpilot.org/packages/focal/libpisock9_0.13.0_amd64.deb
+wget http://www.jpilot.org/packages/focal/libpisync1_0.13.0_amd64.deb
+wget http://www.jpilot.org/packages/focal/pilot-link_0.13.0_amd64.deb
